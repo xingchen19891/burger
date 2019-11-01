b4e795fd8434f1e428c9b199e597fe8a90b937bc
xingchen19891 <50303394+xingchen19891@users.noreply.github.com>
2019-10-31 22:34:48 -0700

    all files added

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('BLT');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('Veggieburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Union', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheeseburger', true);
